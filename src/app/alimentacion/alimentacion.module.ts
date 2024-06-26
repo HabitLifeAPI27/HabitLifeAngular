e1c37c1e9832e304436b4d9b169313fc7c24f428
Meza27 <u202224016@upc.edu.pe>
2024-06-25 22:20:15 -0500

    feat:Componente de Editar Plan Nutricional

--- src/app/alimentacion/alimentacion.module.ts
+++ src/app/alimentacion/alimentacion.module.ts
@@ -10,13 +10,15 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
+import { EditarPlanNutricionalComponent } from './editar-plan-nutricional/editar-plan-nutricional.component';
 
 
 
 @NgModule({
   declarations: [
     AlimentacionComponent,
-    CrearPlanNutricionalComponent
+    CrearPlanNutricionalComponent,
+    EditarPlanNutricionalComponent
   ],
   imports: [
     CommonModule,
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { AlimentacionComponent } from './alimentacion/alimentacion/alimentacion.component';
 import { CrearPlanNutricionalComponent } from './alimentacion/crear-plan-nutricional/crear-plan-nutricional.component';
+import { EditarPlanNutricionalComponent } from './alimentacion/editar-plan-nutricional/editar-plan-nutricional.component';
 
 const routes: Routes = [
   {path: '', component: LandingPageComponent},
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   {path: 'alimentacion', component: AlimentacionComponent },
-  {path: 'crear-plan-nutricional', component: CrearPlanNutricionalComponent}
+  {path: 'crear-plan-nutricional', component: CrearPlanNutricionalComponent},
+  { path: 'editar-plan-nutricional', component: EditarPlanNutricionalComponent}
 ];
 
 @NgModule({
